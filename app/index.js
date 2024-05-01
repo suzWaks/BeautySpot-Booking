7b5e7807bd4f79ed6cbfee695bc82a7a1be9cebc
ThuktenSingye <thuktensingye2163@gmail.com>
2024-05-02 00:54:39 +0600

    render app after font loaded

--- app/index.js
+++ app/index.js
@@ -1,8 +1,42 @@
 /** @format */
 
 import { Link, Redirect } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
+import { FONTS } from "../app/utils/theme.js";
+import { useEffect, useState } from "react";
+import * as SplashScreen from "expo-splash-screen";
+
+const getFonts = () => {
+  return Font.loadAsync({
+    [FONTS.regular]: require("../assets/fonts/Lora Regular.ttf"),
+    [FONTS.medium]: require("../assets/fonts/Lora-Medium.ttf"),
+    [FONTS.semiBold]: require("../assets/fonts/Lora-SemiBold.ttf"),
+    [FONTS.bold]: require("../assets/fonts/Lora-Bold.ttf"),
+  });
+};
+
+SplashScreen.preventAutoHideAsync();
 
 export default function Page() {
-  return <Redirect href={"/Splash"} />;
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        getFonts();
+        await SplashScreen.hideAsync();
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setFontsLoaded(true);
+      }
+    }
+    prepare();
+  }, []);
+
+  if (fontsLoaded) {
+    return <Redirect href={"/Splash"} />;
+  } else {
+    return null;
+  }
 }
