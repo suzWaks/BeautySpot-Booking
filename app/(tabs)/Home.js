0727a1958f3d4630c8e5ee00727ee6720e73b0d7
ThuktenSingye <thuktensingye2163@gmail.com>
2024-05-01 23:49:57 +0600

    updated icon path

--- app/(tabs)/Home.js
+++ app/(tabs)/Home.js
@@ -2,15 +2,15 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import {
-    StyleSheet,
-    Text,
-    View,
-    Pressable,
-    Image,
-    TouchableOpacity,
-    Dimensions,
-    SafeAreaView,
-    ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  Pressable,
+  Image,
+  TouchableOpacity,
+  Dimensions,
+  SafeAreaView,
+  ScrollView,
 } from "react-native";
 import GenderTab from "../components/GenderTab";
 import Icon from "../components/Icon";
@@ -27,113 +27,102 @@ import { SegmentedButtons } from "react-native-paper";
 import ServiceIconList2 from "../components/ServiceIconList2";
 import Testimonials from "../components/Testimonials";
 import { NavigationContainer } from "@react-navigation/native";
-const FirstRoute = () => (
-    <View style={{ flex: 1 }}>
-        <ServiceIconList type={"male"} />
-    </View>
-);
-const SecondRoute = () => (
-    <View style={{ flex: 1 }}>
-        <ServiceIconList type={"female"} />
-    </View>
-);
 
 export default function Home() {
-    const heightValue = useRef(new Animated.Value(0)).current;
+  const heightValue = useRef(new Animated.Value(0)).current;
 
-    const [allService, setAllService] = useState(true);
-    const handleClick = () => {
-        Animated.timing(heightValue, {
-            toValue: allService ? 0 : 10,
-            duration: 50000,
-            useNativeDriver: true,
-        }).start();
-        setAllService(!allService);
-    };
+  const [allService, setAllService] = useState(true);
+  const handleClick = () => {
+    Animated.timing(heightValue, {
+      toValue: allService ? 0 : 10,
+      duration: 50000,
+      useNativeDriver: true,
+    }).start();
+    setAllService(!allService);
+  };
 
-    const [index, setIndex] = useState(0);
-    const [routes] = useState([
-        { key: "first", title: "Male" },
-        { key: "second", title: "Female" },
-    ]);
-    const [value, setValue] = React.useState("male");
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    { key: "first", title: "Male" },
+    { key: "second", title: "Female" },
+  ]);
+  const [value, setValue] = React.useState("male");
 
-    return (
-        <SafeAreaView style={{ flex: 1 }}>
-            <ScrollView style={styles.scrollContainer} scrollEnabled>
-                <View style={styles.container}>
-                    <HomeBanner bookingSuccess={false} />
-                    <Text style={[styles.serviceQuery, { textAlign: "left" }]}>
-                        What are you looking for ?
-                    </Text>
-                    {/* <GenderTab /> */}
-                    <SegmentedButtons
-                        style={{ backgroundColor: "white", borderRadius: 0 }}
-                        value={value}
-                        onValueChange={setValue}
-                        buttons={[
-                            {
-                                value: "male",
-                                label: "Male",
-                                style: {
-                                    borderBottomColor: value === "male" ? "#116CE2" : "white",
-                                    backgroundColor: "white",
-                                    borderColor: "white",
-                                    borderRadius: 0,
-                                },
-                            },
-                            {
-                                value: "female",
-                                label: "Female",
-                                style: {
-                                    borderBottomColor: value === "female" ? "#116CE2" : "white",
-                                    backgroundColor: "white",
-                                    borderColor: "white",
-                                    borderRadius: 0,
-                                },
-                            },
-                        ]}
-                    />
-                    <ServiceIconList2 type={value} allService={allService} />
-                    {allService ? (
-                        <ServiceButton onPress={handleClick} text={"Close All Service"} />
-                    ) : (
-                        <ServiceButton onPress={handleClick} text={"Show all Service"} />
-                    )}
-                    <Testimonials />
-                    {/* <BookingSuccess /> */}
-                    {/* <StatusBar style="auto" /> */}
-                </View>
-            </ScrollView>
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView style={styles.scrollContainer} scrollEnabled>
+        <View style={styles.container}>
+          <HomeBanner bookingSuccess={false} />
+          <Text style={[styles.serviceQuery, { textAlign: "left" }]}>
+            What are you looking for ?
+          </Text>
+          {/* <GenderTab /> */}
+          <SegmentedButtons
+            style={{ backgroundColor: "white", borderRadius: 0 }}
+            value={value}
+            onValueChange={setValue}
+            buttons={[
+              {
+                value: "male",
+                label: "Male",
+                style: {
+                  borderBottomColor: value === "male" ? "#116CE2" : "white",
+                  backgroundColor: "white",
+                  borderColor: "white",
+                  borderRadius: 0,
+                },
+              },
+              {
+                value: "female",
+                label: "Female",
+                style: {
+                  borderBottomColor: value === "female" ? "#116CE2" : "white",
+                  backgroundColor: "white",
+                  borderColor: "white",
+                  borderRadius: 0,
+                },
+              },
+            ]}
+          />
+          <ServiceIconList2 type={value} allService={allService} />
+          {allService ? (
+            <ServiceButton onPress={handleClick} text={"Close All Service"} />
+          ) : (
+            <ServiceButton onPress={handleClick} text={"Show all Service"} />
+          )}
+          <Testimonials />
+          <StatusBar style="auto" />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        margin: 10,
-        padding: 10,
-        fontFamily: "Times New Roman",
-        display: "flex",
-        flexDirection: "column",
-        gap: 20,
-        backgroundColor: "#F4F7FC",
-    },
-    scrollContainer: {
-        flex: 1,
-    },
-    tabcontainer: {
-        marginBottom: 20,
-    },
-    scene: {
-        flex: 1,
-    },
-    serviceQuery: {
-        fontSize: 16,
-        fontWeight: "bold",
-        color: "black",
-        fontFamily: "Times New Roman",
-        textAlign: "left",
-    },
+  container: {
+    flex: 1,
+    margin: 10,
+    padding: 10,
+    fontFamily: "Times New Roman",
+    display: "flex",
+    flexDirection: "column",
+    gap: 20,
+    backgroundColor: "#F4F7FC",
+  },
+  scrollContainer: {
+    flex: 1,
+  },
+  tabcontainer: {
+    marginBottom: 20,
+  },
+  scene: {
+    flex: 1,
+  },
+  serviceQuery: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "black",
+    fontFamily: "Times New Roman",
+    textAlign: "left",
+  },
 });
--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
+/** @format */
+
 import { Link, Redirect } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function Page() {
-  return <Redirect href={'/Splash'} />
+  return <Redirect href={"/Splash"} />;
 }
